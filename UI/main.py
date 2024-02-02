2942fa501332360c46c5bf9930ab12920ceb1bfb
kme-developer <electruc0095@gmail.com>
2024-02-02 12:25:30 +0900

    feat. .env 적용되도록 수정, refactoring. ManageVariables 클래스 생성 및 구조 변경.

--- UI/main.py
+++ UI/main.py
@@ -1,47 +1,78 @@
 import customtkinter as ctk
-from frames.frame_01 import (
-    AmazonIherbOptionFrame,
-    AmazonIDPasswordInputFrame,
-)
-from frames.frame_02 import ImageSizeOptionFrame, BorderColorOptionFrame
-
-font = "돋움"
-font_size = 13
-
-thumbnail_color = "white"
 
 
-class MainUI(ctk.CTk):
-    def __init__(self):
-        super().__init__()
+class MainUI:
+    def __init__(
+        self,
+        font,
+        font_size,
+    ):
+        self.font = font
+        self.font_size = font_size
+        self.manageVaribles = ManageVariables()
 
-        self.title("KME Scraper")
-
-        # self.protocol("WM_DELETE_WINDOW", self.open_toplevel)
+    def start(self):
+        root = ctk.CTk()
+        root.title("KME Scraper")
 
         ctk.set_appearance_mode("dark")
         ctk.set_default_color_theme("blue")
 
-        self.font_style = ctk.CTkFont(font, size=font_size)
+        font_style = ctk.CTkFont(self.font, size=self.font_size)
 
         """
         frame 01
         """
-        frame_01 = ctk.CTkFrame(self)
+        frame_01 = ctk.CTkFrame(root)
         frame_01.grid(row=0, column=0, padx=(10, 5), pady=(10, 5), sticky="new")
 
-        AmazonIherbOptionFrame(frame_01, self.font_style)
-        AmazonIDPasswordInputFrame(frame_01, self.font_style)
+        option_frame = ctk.CTkFrame(frame_01)
+        option_frame.pack(side="left", fill="both", expand=True, padx=(10, 5), pady=10)
 
-        """
-        frame 02
-        """
-        frame_02 = ctk.CTkFrame(self)
-        frame_02.grid(row=1, column=0, padx=(10, 5), pady=5, sticky="new")
+        amazon_iherb_segemented_button = ctk.CTkSegmentedButton(
+            option_frame,
+            font=font_style,
+            values=[
+                " 아마존 ",
+                " 아이허브 ",
+            ],
+            command=self.manageVaribles.update_amazon_iherb_option,
+        )
+        amazon_iherb_segemented_button.set(" 아마존 ")
+        amazon_iherb_segemented_button.pack(expand=True, padx=10, pady=10)
 
-        ImageSizeOptionFrame(frame_02, self.font_style)
-        BorderColorOptionFrame(frame_02, self.font_style)
+        entry_frame = ctk.CTkFrame(frame_01)
+        entry_frame.pack(fill="both", side="right", expand=True, padx=(5, 10), pady=10)
+
+        # Mobile phone number or email = entry
+        id_entry = ctk.CTkEntry(
+            entry_frame,
+            placeholder_text=" Mobile phone number or email",
+            font=font_style,
+        )
+        id_entry.pack(side="top", fill="x", expand=True, padx=20, pady=(10, 2.5))
+        # Password = entry
+        password_entry = ctk.CTkEntry(
+            entry_frame, placeholder_text=" Password", font=font_style
+        )
+        password_entry.pack(
+            side="bottom", fill="x", expand=True, padx=20, pady=(2.5, 10)
+        )
+
+        root.mainloop()
 
 
-mainUI = MainUI()
-mainUI.mainloop()
+class ManageVariables:
+    def __init__(self, amazon_iherb_option="amazon", thumbnail_border_color="white"):
+        self.amazon_iherb_option = amazon_iherb_option
+        self.thumbnail_border_color = thumbnail_border_color
+
+    def update_amazon_iherb_option(self, value: str):
+        if value.strip() == "아마존":
+            amazon_iherb_option = "amazon"
+        else:
+            amazon_iherb_option = "iherb"
+        print(amazon_iherb_option)
+
+    def get_amazon_iherb_option(self):
+        return self.amazon_iherb_option
--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from mac.checker import MACAddressChecker
 from ui.warning import WarningUI
+from ui.main import MainUI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 default_set_date = "2024-02-01"
 days = 30
-default_mac_address = "40-B0-76-42-8F-**"
+default_mac_address = os.getenv("DEFAULT_MAC_ADDRESS")
 
 warning_message_access_denied = "인증된 사용자가 아닙니다."
 warning_message_expired = "사용 기간이 만료되었습니다."
+
+font = "돋움"
+font_size = 13
 
 
 class App:
@@ -15,14 +24,15 @@ class App:
             default_set_date, days, default_mac_address
         )
         self.access_denied = WarningUI(warning_message_access_denied)
+        self.main = MainUI(font, font_size)
         self.expired = WarningUI(warning_message_expired)
 
     def start(self):
         if self.macAddressChecker.is_default_mac_address():
             if self.macAddressChecker.is_expired():
-                return "..."
-            else:
                 self.expired.show_warning()
+            else:
+                self.main.start()
         else:
             self.access_denied.show_warning()
 
